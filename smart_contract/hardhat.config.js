9199e3d7d7443a528d1b58d13088a0ba30d78c8e
PRC2004 <parth.prc.95@gmail.com>
2024-10-23 15:23:21 +0530

    Changes made to config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.0', //Not to Touch for Now.
   networks: {
     ropsten: {
-      url: 'https://eth-sepolia.g.alchemy.com/v2/fpm52gHrh5D5AFxbKQUa25UF7ASKxUtZ', //This Done.
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+      url: 'https://eth-sepolia.g.alchemy.com/v2/fpm52gHrh5D5AFxbKQUa25UF7ASKxUtZ', //This is the Url from alchemy.com
+      accounts: ['342f9ee17e6685e7ef1d1808ffafe0c6542848445b0eec925c38094ae80302cd'], //This is my MetaMask Private Key
     },
   },
 };
