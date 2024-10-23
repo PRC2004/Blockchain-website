297648ef754fd01bc9e8fd73adbaf95d52a5ce7e
PRC2004 <parth.prc.95@gmail.com>
2024-10-23 16:02:44 +0530

    Correcting Code

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,5 @@
 import abi from "./Transactions.json";
-export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; //My Transaction Address
+export const contractAddress = "0x59E686Aa27A84FF39a1f8394539BAbE64d948E62"; //My Transaction Address
 export const contractABI = abi.abi;
 
 // export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,7 +3,7 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0', //Not to Touch for Now.
   networks: {
-    ropsten: {
+    sepolia: {
       url: 'https://eth-sepolia.g.alchemy.com/v2/fpm52gHrh5D5AFxbKQUa25UF7ASKxUtZ', //This is the Url from alchemy.com
       accounts: ['342f9ee17e6685e7ef1d1808ffafe0c6542848445b0eec925c38094ae80302cd'], //This is my MetaMask Private Key
     },
